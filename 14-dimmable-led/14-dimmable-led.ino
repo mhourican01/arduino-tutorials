7a1efed91080a1a11bc8088c279e9dd93a5f20bf
mhourican01 <mhourican01@qub.ac.uk>
2021-01-28 09:59:50 +0000

    Controlled LED brightness with potentiometer

---
+++ 14-dimmable-led/14-dimmable-led.ino
@@ -0,0 +1,19 @@
+int potPin = A1;
+int ledPin = 6;
+int potVal;
+float ledVal;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(potPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  potVal = analogRead(potPin);
+  ledVal = (255./1023.) * potVal;
+  analogWrite(ledPin, ledVal);
+  Serial.println(ledVal);
+}

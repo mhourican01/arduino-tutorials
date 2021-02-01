6eb478da20c3a267ca7a1d00a6995dc322119598
mhourican01 <mhourican01@qub.ac.uk>
2021-02-01 11:23:44 +0000

    User can toggle LED with pushbutton

---
+++ 28-toggle-led/28-toggle-led.ino
@@ -0,0 +1,31 @@
+int ledState = 0;
+int ledPin = 8;
+int buttonPin = 12;
+int buttonNew;
+int buttonOld = 1;
+int dT = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonNew = digitalRead(buttonPin);
+  Serial.println(buttonNew);
+  
+  if (buttonOld == 0 and buttonNew == 1) {
+    if (ledState == 0) {
+      digitalWrite(ledPin, HIGH);
+      ledState = 1;
+    } else {
+      digitalWrite(ledPin, LOW);
+      ledState = 0;
+    }
+  }
+
+  buttonOld = buttonNew;
+}

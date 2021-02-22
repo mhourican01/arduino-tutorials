77154be99d57f7637017c00636c4f34df404855a
mhourican01 <mhourican01@qub.ac.uk>
2021-02-22 13:48:45 +0000

    Prints numbers in their decimal, binary, and hexadecimal forms

---
+++ 41-hexadecimal-writer/41-hexadecimal-writer.ino
@@ -0,0 +1,18 @@
+byte myByte = 0x00;
+int dT = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(myByte, DEC);
+  Serial.print("  ");
+  Serial.print(myByte, BIN);
+  Serial.print("  ");
+  Serial.println(myByte, HEX);
+  myByte = myByte + 1;
+  delay(dT);
+}

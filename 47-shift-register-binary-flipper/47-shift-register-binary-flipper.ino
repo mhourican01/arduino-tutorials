10cf6407a75b9b4ecbae70bc08d318731875c4f9
mhourican01 <mhourican01@qub.ac.uk>
2021-02-23 09:58:30 +0000

    Coded shift register to control LED display

--- 47-shift-register-binary-flipper/47-shift-register-binary-flipper.ino
+++ 47-shift-register-binary-flipper/47-shift-register-binary-flipper.ino
@@ -1,13 +1,12 @@
-  
 int latchPin = 11;
 int dataPin = 12;
 int clockPin = 9;
-byte LEDs = 0b00000000;
+byte myByte = 0b01010101;
+byte myByteFlipped;
 int dT = 500;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
@@ -16,8 +15,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, LSBFIRST, LEDs);
+  shiftOut(dataPin, clockPin, LSBFIRST, myByte);
   digitalWrite(latchPin, HIGH);
   delay(dT);
-  LEDs += 1;
+  
+  myByteFlipped = 255 - myByte;
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, myByteFlipped);
+  digitalWrite(latchPin, HIGH);
+  delay(dT);
 }

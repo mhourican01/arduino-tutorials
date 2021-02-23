bc0a2c809359c07688c825f6ddd2ad036ab9f620
mhourican01 <mhourican01@qub.ac.uk>
2021-02-23 10:43:25 +0000

    LCD displays count from 1 to 10, dimmable by potentiometer

---
+++ 48-lcd-counter/48-lcd-counter.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+int rs = 7;
+int en = 8;
+int d4 = 9;
+int d5 = 10;
+int d6 = 11;
+int d7 = 12;
+int dT = 500;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,0);
+  lcd.print("Count:");
+  for (int count = 1; count <= 10; count++) {
+    lcd.setCursor(0,1);
+    lcd.print(count);
+    delay(dT);
+  }
+  lcd.clear();
+}
